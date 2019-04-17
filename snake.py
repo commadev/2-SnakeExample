d574ace794a88585f336e783a8ca74c66bc77a10
DeCrazy <47843542+DeCrazy@users.noreply.github.com>
2019-04-17 10:32:24 +0900

    일단 센서만넣음

--- snake.py
+++ snake.py
@@ -2,26 +2,37 @@ import math
 import random
 import pygame
 import tkinter as tk
+import numpy as np
 from tkinter import messagebox
 
-class cube(object):
+DIRECTIONS = np.array([ #움직이기편하게 디렉션만들어줌 (이거수정좀)
+    (0, -1), #UP     0
+    (0, 1),  #DOWN   1
+    (-1, 0), #LEFT   2
+    (1, 0)   #RIGHT  3
+])
+
+class Cube(object):
     rows = 20
     width = 500
     def __init__(self,start,color=(255,0,0)):
         self.pos = start
         self.color = color
+        self.score = 0
         
     def move(self, x, y):
         self.pos = (self.pos[0] + x, self.pos[1] + y)  # change our position
+
+    def getpos(self):
+        return self.pos
     
     def draw(self, surface):
-        dis = self.width // self.rows  # Width/Height of each cube
+        dis = self.width // self.rows  # Width/Height of each Cube
         i = self.pos[0] # Current row
         j = self.pos[1] # Current Column
 
         pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
-        # By multiplying the row and column value of our cube by the width and height of each cube we can determine where to draw it
-
+        # By multiplying the row and column value of our Cube by the width and height of each Cube we can determine where to draw it
 
 
 def move(cube1, cube2):
@@ -29,40 +40,40 @@ def move(cube1, cube2):
             if event.type == pygame.QUIT:
                 pygame.quit()
             keys = pygame.key.get_pressed()
-
+            if __name__ == '__main__': 
             #Key Event
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    cube1.move(-1, 0)
-                    break
+                for key in keys:
+                    if keys[pygame.K_LEFT]:
+                        cube1.move(DIRECTIONS[2])
+                        break
 
-                if keys[pygame.K_RIGHT]:
-                    cube1.move(1, 0)
-                    break
+                    if keys[pygame.K_RIGHT]:
+                        cube1.move(DIRECTIONS[3])
+                        break
                     
-                if keys[pygame.K_UP]:
-                    cube1.move(0, -1)
-                    break
+                    if keys[pygame.K_UP]:
+                        cube1.move(DIRECTIONS[0])
+                        break
 
-                if keys[pygame.K_DOWN]:
-                    cube1.move(0, 1)
-                    break
+                    if keys[pygame.K_DOWN]:
+                        cube1.move(DIRECTIONS[1])
+                        break
 
-                if keys[pygame.K_a]:
-                    cube2.move(-1, 0)
-                    break
+                    if keys[pygame.K_a]:
+                        cube2.move(-1, 0)
+                        break
 
-                if keys[pygame.K_d]:
-                    cube2.move(1, 0)
-                    break
-                    
-                if keys[pygame.K_w]:
-                    cube2.move(0, -1)
-                    break
+                    if keys[pygaame.K_d]:
+                        cube2.move(1, 0)
+                        break
 
-                if keys[pygame.K_s]:
-                    cube2.move(0, 1)
-                    break
+                    if keys[pygame.K_w]:
+                        cube2.move(0, -1)
+                        break
+
+                    if keys[pygame.K_s]:
+                        cube2.move(0, 1)
+                        break
 
 
 def drawGrid(surface):
@@ -94,6 +105,26 @@ def randomSnack(rows):
     y = random.randrange(rows)
     return x, y
 
+def sensor(self, Cube1, Cube2):     #센서 달아주는부분
+    self.cube_pos = Cube1.getpos
+    self.item_pos = Cube2.getpos
+
+    input_layer = [0.0, 0.0, 0.0, 0.0]  #입력층
+    output_layer = [0.0, 0.0, 0.0, 0.0] #출력층
+
+    for i in range(5):                  #먹이찾기
+        if cube_pos[0] + i == item_pos:
+            input_layer[0] *= 0.2 * i
+        if cube_pos[0] - i == item_pos:
+            input_layer[1] *= 0.2 * i
+        if cube_pos[1] + i == item_pos:
+            input_layer[2] *= 0.2 * i
+        if cube_pos[1] - i == item_pos:
+            input_layer[3] *= 0.2 * i
+
+
+    
+
 
 def main(): 
     global width, rows, cube1, cube2, snack1, snack2
@@ -103,14 +134,12 @@ def main():
 
     win = pygame.display.set_mode((width, height))  # Creates our screen object
 
-    cube1 = cube((10,10), (255,0,0))  # Creates a snake object which we will code later
-    cube2 = cube((10,12), (0,0,255))  
-    snack1 = cube(randomSnack(rows), color=(255,0,255))
-    snack2 = cube(randomSnack(rows), color=(0,255,255))
+    cube1 = Cube((10,10), (255,0,0))  # Creates a snake object which we will code later
+    cube2 = Cube((10,12), (0,0,255))  
+    snack1 = Cube(randomSnack(rows), color=(255,0,255))
+    snack2 = Cube(randomSnack(rows), color=(0,255,255))
   
     clock = pygame.time.Clock() # creating a clock object
-
-    
 
     flag = True
     # STARTING MAIN LOOP
@@ -122,10 +151,21 @@ def main():
 
         #Collision Check
         if cube1.pos == snack1.pos:
-            snack1 = cube(randomSnack(rows), color=(255,0,255))
+            snack1 = Cube(randomSnack(rows), color=(255,0,255))
+            cube1.score += 1
+            print("Cube 1",cube1.score,"Point!")
         if cube2.pos == snack2.pos:
-            snack2 = cube(randomSnack(rows), color=(0,255,255))
+            snack2 = Cube(randomSnack(rows), color=(0,255,255))
+            cube2.score += 1
+            print("Cube 2",cube2.score,"Point!")
+        if cube1.pos == snack2.pos:
+            snack2 = Cube(randomSnack(rows), color=(0,255,255))
+            cube1.score -= 2
+            print("Cube 1",cube1.score,"Point!")
+        if cube2.pos == snack1.pos:
+            snack1 = Cube(randomSnack(rows), color=(0,255,255))
+            cube2.score -= 2
+            print("Cube 2",cube2.score,"Point!")
 
-
-
+#if __name__ != '__main__': # 활성화부분 아직뺌
 main()
