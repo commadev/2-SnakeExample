95ab2512e1da91f89a1ee41b0ea6b3123850ac98
Imakara <Imakara38@users.noreply.github.com>
2019-04-30 02:41:26 +0900

    [feat] 스네이크 충돌 구현
    추가
    + 충돌시 스네이크 리셋
    
    수정
    * myBlockList 찌꺼기 제거

--- snake.py
+++ snake.py
@@ -23,11 +23,11 @@ color_snake = [(255,0,0),(0,255,0),(0,0,255),(255,255,0)]
 
 
 class cube(object):
-    def __init__(self,start,color):
+    def __init__(self,start,color, odd):
         self.pos = start
         self.color = color
         self.hp = 100
-        self.odd = random.random() / 2 + 0.5
+        self.odd = odd
         block_list.append(start)
         
     def move(self, x, y):
@@ -41,12 +41,9 @@ class cube(object):
         dis = width // rows  # Width/Height of each cube
         i = self.pos[0] # Current row
         j = self.pos[1] # Current Column
-
         pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
         # By multiplying the row and column value of our cube by the width and height of each cube we can determine where to draw it
-
-
-
+ 
 def drawGrid(surface):
     sizeBtwn = width // rows  # Gives us the distance between the lines
 
@@ -74,55 +71,59 @@ def randomPos(rows):
     y = random.randrange(rows)
     return x, y
 
-def item_sensor(cube, item):     #Sensor
+def item_sensor(snake_, snack_):     #Sensor
     global block_list
     myBlocklist = []
     input_layer = [0,0,0,0]
     output_layer = ["Up","Left","Down","Right"]
     myBlocklist[:] = block_list[:]
-    myBlocklist.remove(cube.pos)
-    myBlocklist.remove(item.pos)
+    myBlocklist.remove(snake_.pos)
+    myBlocklist.remove(snack_.pos)
+    #print(myBlocklist)
+    #print(block_list)
 
     #Find_item
-    if cube.pos[1] > item.pos[1]:
-        input_layer[0] = cube.pos[1] - item.pos[1]
-    if cube.pos[0] > item.pos[0]:
-        input_layer[1] = cube.pos[0] - item.pos[0]
-    if cube.pos[1] < item.pos[1]:
-        input_layer[2] = item.pos[1] - cube.pos[1]
-    if cube.pos[0] < item.pos[0]:
-        input_layer[3] = item.pos[0] - cube.pos[0]
+    if snake_.pos[1] > snack_.pos[1]:
+        input_layer[0] = snake_.pos[1] - snack_.pos[1]
+    if snake_.pos[0] > snack_.pos[0]:
+        input_layer[1] = snake_.pos[0] - snack_.pos[0]
+    if snake_.pos[1] < snack_.pos[1]:
+        input_layer[2] = snack_.pos[1] - snake_.pos[1]
+    if snake_.pos[0] < snack_.pos[0]:
+        input_layer[3] = snack_.pos[0] - snake_.pos[0]
 
-    
-    for i in range(len(block_list)):
-        if cube.pos[1] + 1 == block_list[i][1]:
-            input_layer[0] -= (cube.pos[1] - block_list[i][1]) * cube.odd
-        if cube.pos[0] + 1 == block_list[i][0]:
-            input_layer[1] -= (cube.pos[0] - block_list[i][0]) * cube.odd
-        if cube.pos[1] - 1 == block_list[i][1]:
-            input_layer[2] -= (block_list[i][1] - cube.pos[1]) * cube.odd
-        if cube.pos[0] - 1 == block_list[i][0]:
-            input_layer[3] -= (block_list[i][0] - cube.pos[0]) * cube.odd
-    '''
-    for i in range(len(block_list)):
-        if cube.pos == block_list[i]:
-    '''
+    #Avoid_block
+    for i in range(len(myBlocklist)):
+        if snake_.pos[1] + 1 == myBlocklist[i][1]:
+            input_layer[0] -= (snake_.pos[1] - myBlocklist[i][1]) * snake_.odd
+        if snake_.pos[0] + 1 == myBlocklist[i][0]:
+            input_layer[1] -= (snake_.pos[0] - myBlocklist[i][0]) * snake_.odd
+        if snake_.pos[1] - 1 == myBlocklist[i][1]:
+            input_layer[2] -= (myBlocklist[i][1] - snake_.pos[1]) * snake_.odd
+        if snake_.pos[0] - 1 == myBlocklist[i][0]:
+            input_layer[3] -= (myBlocklist[i][0] - snake_.pos[0]) * snake_.odd
+    #print(myBlocklist)
+    print(snake_.odd)
+
+    for i in range(len(myBlocklist)):
+        if snake_.pos == myBlocklist[i]:
+            return True
+
 
     #print(input_layer)
     
-    
-
 
     if output_layer[input_layer.index(max(input_layer))] == "Up":
-        cube.move(0, -1)
+        snake_.move(0, -1)
     elif output_layer[input_layer.index(max(input_layer))] == "Left":
-        cube.move(-1, 0)
+        snake_.move(-1, 0)
     elif output_layer[input_layer.index(max(input_layer))] == "Down":
-        cube.move(0, 1)
+        snake_.move(0, 1)
     elif output_layer[input_layer.index(max(input_layer))] == "Right":
-        cube.move(1, 0)
+        snake_.move(1, 0)
     
 
+    return False
 
 def main(): 
     # Creates Screen
@@ -130,10 +131,10 @@ def main():
 
     #Snack
     for i in range(4):
-        snake_list.append(cube(randomPos(rows),color_snake[i]))
+        snake_list.append(cube(randomPos(rows),color_snake[i], random.random()))
     #Snake
     for i in range(4):
-        snack_list.append(cube(randomPos(rows),color_snack[i]))
+        snack_list.append(cube(randomPos(rows),color_snack[i], random.random()))
 
 
     #Creating a clock object
@@ -148,25 +149,31 @@ def main():
         pygame.time.delay(50)  # This will delay the game so it doesn't run too quickly
         clock.tick(10)  # Will ensure our game runs at 10 FPS
 
-
+        #Create Sensor
+        for i in range(len(snake_list)):
+            if(item_sensor(snake_list[i], snack_list[i])):
+                print( str(i) + "   " + str(snake_list[i].odd))
+                block_list.remove(snake_list[i].pos)
+                snake_list[i] = cube(randomPos(rows), color_snake[i], random.random())
+ 
             
         #Collision Check
         for i in range(4):
-            snake_list[i].hp -= 2
+            snake_list[i].hp -= 0
             if snake_list[i].hp <= 0:
                 print( str(i) + "   " + str(snake_list[i].odd))
-                #odd = random.random() / 2 + 0.5
-                snake_list[i] = cube(randomPos(rows), color_snake[i])
+                block_list.remove(snake_list[i].pos)
+                snake_list[i] = cube(randomPos(rows), color_snake[i], random.random())
+            
             if snake_list[i].pos == snack_list[i].pos:
                 snake_list[i].hp = 100
                 block_list.remove(snack_list[i].pos)
-                snack_list[i] = cube(randomPos(rows), color_snack[i])
+                snack_list[i] = cube(randomPos(rows), color_snack[i], random.random())
+            
                 
 
 
-        #Create Sensor
-        for i in range(len(snake_list)):
-            item_sensor(snake_list[i], snack_list[i])
+
             
 
         redrawWindow(win)  # This will refresh our screen
