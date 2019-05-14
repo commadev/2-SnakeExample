e51b869d8b1704ff784dae5ac1556abccd143657
DeCrazy <47843542+DeCrazy@users.noreply.github.com>
2019-05-14 21:40:16 +0900

    Update snake.py
    add socket

--- snake.py
+++ snake.py
@@ -1,13 +1,31 @@
 import math
 import random
 import pygame
+from socket import *
+
 
 
 width = 500  # Width of our screen
 height = 500  # Height of our screen
 rows = 20  # Amount of rows
 
+"""
+소켓 생성과 연결 부분
+"""
 
+serverSock = socket(AF_INET, SOCK_STREAM)
+serverSock.bind(('', 8080))
+serverSock.listen(1)
+
+connectionSock, addr = serverSock.accept()
+print(str(addr),'에서 접속이 확인되었습니다.')
+
+"""
+데이터 보내는 코드입니다
+while True:
+        sendData = '[0, 0, 0]'
+        sock.send(sendData.encode('utf-8')) 인코딩을 utf-8
+"""
 
 #Create List
 snake_list = []
